a601d131b096cdde911492ef719262656beb1923
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-01 20:44:13 +0200

    removed host from log

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,7 @@ const dateString = require('../components/dateString.js');
 
 router.route('/').get(async (req, res) => {
   try {
-    console.log(dateString(), '-', req.method, req.originalUrl, req.headers.host);
+    console.log(dateString(), '-', req.method, req.originalUrl);
     let data = await images(req.query.page);
 
     res.set('Access-Control-Allow-Origin', '*');
--- routes/static.js
+++ routes/static.js
@@ -3,7 +3,7 @@ const static = require('../controllers/images.js');
 const dateString = require('../components/dateString.js');
 
 router.route('/Discord').get(async (req, res) => {
-  console.log(dateString(), '-', req.method, req.originalUrl, req.headers.host);
+  console.log(dateString(), '-', req.method, req.originalUrl);
   let data = await static(req.originalUrl);
   res.status(data.statuscode);
   if (data.statuscode === 200) res.type(data.type);
