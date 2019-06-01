0f2eaf14ae2ad55376d3d7f485ff1d88aaa4efae
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-01 20:39:36 +0200

    added path to route

--- routes/static.js
+++ routes/static.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const static = require('../controllers/images.js');
 const dateString = require('../components/dateString.js');
 
-router.route('/').get(async (req, res) => {
+router.route('/Discord').get(async (req, res) => {
   console.log(dateString(), '-', req.method, req.originalUrl, req.headers.host);
   let data = await static(req.originalUrl);
   res.status(data.statuscode);
