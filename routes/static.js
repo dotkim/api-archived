6274591dbc7c527148b3e876c198060dc27316b8
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-05 20:41:28 +0200

    moved setheaders to middleware

--- routes/static.js
+++ routes/static.js
@@ -4,7 +4,6 @@ const dateString = require('../components/dateString.js');
 
 router.route('/Discord').get(async (req, res) => {
   let data = await static(req.originalUrl);
-  res.set('Access-Control-Allow-Origin', '*');
   res.status(data.statuscode);
   if (data.statuscode === 200) res.type(data.type);
   res.end();
