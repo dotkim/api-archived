95b8ff12e826c0d5e3eff1473d291299cd3f9b21
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-05 22:31:00 +0200

    split the routes.

--- routes/static.js
+++ routes/static.js
@@ -3,15 +3,31 @@ const static = require('../controllers/images.js');
 const dateString = require('../components/dateString.js');
 
 router.route('/Discord').get(async (req, res) => {
-  let data = await static(req.originalUrl);
-  res.status(data.statuscode);
-  if (data.statuscode === 200) res.type(data.type);
-  res.end();
-}).route('/thumbnails').get(async (req, res) => {
-  let data = await static(req.originalUrl);
-  res.status(data.statuscode);
-  if (data.statuscode === 200) res.type(data.type);
-  res.end();
+  try {
+    let data = await static(req.originalUrl);
+    res.status(data.statuscode);
+    if (data.statuscode === 200) res.type(data.type);
+    res.end();
+  }
+  catch (error) {
+    console.error(dateString(), '- got error');
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+router.route('/thumbnails').get(async (req, res) => {
+  try {
+    let data = await static(req.originalUrl);
+    res.status(data.statuscode);
+    if (data.statuscode === 200) res.type(data.type);
+    res.end();
+  }
+  catch (error) {
+    console.error(dateString(), '- got error');
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
