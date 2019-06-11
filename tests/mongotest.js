7f401ffa15ff474bcc7fca8c4067e9297bff1ac8
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-11 21:56:14 +0200

    Create mongotest.js

---
+++ tests/mongotest.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+let conn = mongoose.connect(
+  process.env.MONGOOSE_MONGOURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true, // use this to remove the warning: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+    //user: process.env.MONGOOSE_USERNAME,
+    //pass: process.env.MONGOOSE_PASSWORD,
+    dbName: process.env.MONGOOSE_DBNAME
+  },
+  function (err) {
+    if (err) console.error('Failed to connect to mongo', err); // this might be changed to do some better errorhandling later...
+  }
+);
+
+
+const imageSchema = new Schema(
+  {
+    'fileName': String,
+    'contentType': String,
+    'extension': String,
+    'url': String,
+    'thumbnail': String,
+    'tags': Array
+  },
+  { timestamps: true }
+);
+
+let images = mongoose.model('image', imageSchema);
+
+images.find({ 'tags': { $nin: [ 'tagme'] } }).then(console.log).catch(console.error);
