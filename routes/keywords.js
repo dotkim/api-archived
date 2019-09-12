aa357a9858cba3dfd3a6eff632ea19af19e1feb2
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-09-12 22:23:25 +0200

    added keyword functionality
    Also added try catches to the mongo functions. These needs to be properly handled in the controller as well.

---
+++ routes/keywords.js
@@ -0,0 +1,34 @@
+'use strict';
+const router = require('express').Router();
+const keywordController = require('../controllers/keywords.js');
+const dateString = require('../components/dateString.js');
+const keyword = new keywordController();
+
+router.route('/:word').get(async (req, res) => {
+  try {
+    let data = await keyword.getKeyword(req.params.word);
+    res.status(data.statuscode);
+    if (data.statuscode === 200) res.json(data.content);
+    res.end();
+  }
+  catch (error) {
+    console.error(dateString(), '- got error');
+    console.error(error);
+    res.sendStatus(500);
+  }
+}).post(async (req, res) => {
+  try {
+    let data = await keyword.addKeyword(req.params.word, req.body.message);
+    
+    res.status(data.statuscode);
+    if (data.statuscode === 200) res.json(data.content);
+    res.end();
+  }
+  catch (error) {
+    console.error(dateString(), '- got error');
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
