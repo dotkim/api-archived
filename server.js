3aab7e215d18e0d0b8da608b0c494bc9fbd54951
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-02 22:02:53 +0200

    added switch for http/https

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 'use strict';
 require('dotenv').config();
 const dateString = require('./components/dateString.js');
-const https = require('https');
 const fs = require('fs');
 const app = require('./app');
 
 console.log('############### WEB SERVER START UP ###############');
 console.log(dateString(), '- starting http server');
 
-const httpsCert = process.env.HTTPS_CERT;
-const httpsKey = process.env.HTTPS_KEY;
-const httpsPort = process.env.HTTPS_PORT || 443;
+if (process.env.USE_HTTPS === "false") {
+  const http = require('http');
+  const httpPort = process.env.HTTP_PORT;
 
-const options = {
-  key: fs.readFileSync(httpsKey),
-  cert: fs.readFileSync(httpsCert)
-};
+  http.createServer(app).listen(httpPort);
+}
+else {
+  const https = require('https');
+  const httpsCert = process.env.HTTPS_CERT;
+  const httpsKey = process.env.HTTPS_KEY;
+  const httpsPort = process.env.HTTPS_PORT || 443;
+  
+  const options = {
+    key: fs.readFileSync(httpsKey),
+    cert: fs.readFileSync(httpsCert)
+  };
+  
+  https.createServer(options, app).listen(httpsPort);
+}
 
-https.createServer(options, app).listen(httpsPort);
 console.log(dateString(), '- listening on port', httpsPort);
