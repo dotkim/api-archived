375cd8ac7469b308dd0edd9f4fdb0f807e685229
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-05 20:21:25 +0200

    changed CORS headers

--- data/configuration.js
+++ data/configuration.js
@@ -22,6 +22,11 @@ function Config() {
 
   this.config.authUser = process.env.AUTH_USER;
   this.config.authPass = process.env.AUTH_PASS;
+  
+  this.config.accessControlOrigin = process.env.ACCESS_CTRL_ORIGIN || '*';
+  this.config.accessControlMethods = process.env.ACCESS_CTRL_METHODS || 'GET, POST, DELETE, PUT';
+  this.config.accessControlHeaders = process.env.ACCESS_CTRL_HEADERS || '*';
+  this.config.accessControlCredentials = process.env.ACCESS_CTRL_CREDENTIALS || false;
 }
 
 Config.call(this);
