f0f06e2c64184ec174954088e020041d5fdd7fc3
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-14 17:37:34 +0200

    Comments

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 'use strict';
 const express = require('express');
-
 const jsonParser = require('body-parser').json({
   type: 'application/json', extended: true, limit: '50mb'
 });
-
 const authentication = require('./security/authentication');
-
 const app = new express();
-app.disable('x-powered-by');
-app.use(jsonParser);
 
-// Global logging
+// clean up powered by header
+app.disable('x-powered-by');
+
+// Use middleware and other
+app.use(jsonParser);
 app.use(require('./middleware/headers'));
 app.use(require('./middleware/logging'));
 
+// Routes without authtentication
 app.use('/images', require('./routes/images'));
-app.use('/insert', authentication(), require('./routes/insert'));
-app.use('/keyword', authentication(), require('./routes/keywords'));
 app.use('/triggers', require('./routes/triggers'));
 app.use('/videos', require('./routes/videos'));
+
+// Routes that require authentication
+app.use('/insert', authentication(), require('./routes/insert'));
+app.use('/keyword', authentication(), require('./routes/keywords'));
 
 module.exports = app;
