40a8865741d99c786f63fd671684eac03b688a6e
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-31 00:02:51 +0100

    updated packages

--- models/db.js
+++ models/db.js
@@ -20,6 +20,9 @@ module.exports = class {
       // use this to remove the warning:
       // DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
       useFindAndModify: false,
+      // DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version.
+      //To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
+      useUnifiedTopology: true,
       //user: process.env.MONGOOSE_USERNAME,
       //pass: process.env.MONGOOSE_PASSWORD,
       dbName: config.mongooseDbname
