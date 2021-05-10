7e66fd04517921eba2ae73cf3e990271ec2742a6
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2021-05-10 21:32:39 +0200

    Remove old

--- models/keyword.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const { Schema } = require('mongoose');
-  const keywordSchema = new Schema({
-    'keyword': String,
-    'values': Array
-  });
-
-  module.exports = keywordSchema;
--- security/getUnauthorizedResponse.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-//eslint-disable-next-line arrow-body-style
-const getUnauthorizedResponse = (req) => {
-  return req.auth
-    ? '<h1>401 Unauthorized</h1><p>Credentials rejected</p>'
-    : '<h1>401 Unauthorized</h1><p>No credentials provided</p>'
-}
-
-module.exports = getUnauthorizedResponse;
