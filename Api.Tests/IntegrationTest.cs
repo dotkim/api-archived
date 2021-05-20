cf92b93de6bacd5797c67e498547de66dac415d7
dotkim <kim.andre.nerli@gmail.com>
2021-05-20 23:01:26 +0200

    auto formatted.

--- Api.Tests/IntegrationTest.cs
+++ Api.Tests/IntegrationTest.cs
@@ -6,40 +6,40 @@ using Api.ServiceModel;
 
 namespace Api.Tests
 {
-    public class IntegrationTest
+  public class IntegrationTest
+  {
+    const string BaseUri = "http://localhost:2000/";
+    private readonly ServiceStackHost appHost;
+
+    class AppHost : AppSelfHostBase
     {
-        const string BaseUri = "http://localhost:2000/";
-        private readonly ServiceStackHost appHost;
+      public AppHost() : base(nameof(IntegrationTest), typeof(ImageService).Assembly) { }
 
-        class AppHost : AppSelfHostBase
-        {
-            public AppHost() : base(nameof(IntegrationTest), typeof(ImageService).Assembly) { }
-
-            public override void Configure(Container container)
-            {
-            }
-        }
-
-        public IntegrationTest()
-        {
-            appHost = new AppHost()
-                .Init()
-                .Start(BaseUri);
-        }
-
-        [OneTimeTearDown]
-        public void OneTimeTearDown() => appHost.Dispose();
-
-        public IServiceClient CreateClient() => new JsonServiceClient(BaseUri);
-
-        [Test]
-        public void Can_call_Image_Service()
-        {
-            var client = CreateClient();
-
-            var response = client.Get(new GetImageRandom { GuildId = 1234 });
-
-            //Assert.That(response.Result, Is.EqualTo("Hello, World!"));
-        }
+      public override void Configure(Container container)
+      {
+      }
     }
+
+    public IntegrationTest()
+    {
+      appHost = new AppHost()
+          .Init()
+          .Start(BaseUri);
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() => appHost.Dispose();
+
+    public IServiceClient CreateClient() => new JsonServiceClient(BaseUri);
+
+    [Test]
+    public void Can_call_Image_Service()
+    {
+      var client = CreateClient();
+
+      var response = client.Get(new GetImageRandom { GuildId = 1234 });
+
+      //Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+    }
+  }
 }
