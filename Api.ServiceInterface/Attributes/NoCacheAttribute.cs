3ee6ef245906bc7bc81ce6322f2e024e04490cf5
dotkim <kim.andre.nerli@gmail.com>
2021-05-25 10:16:37 +0000

    Add NoCacheAttribute

---
+++ Api.ServiceInterface/Attributes/NoCacheAttribute.cs
@@ -0,0 +1,13 @@
+using ServiceStack;
+using ServiceStack.Web;
+
+namespace Api.Attributes
+{
+  public class NoCacheAttribute : RequestFilterAttribute
+  {
+    public override void Execute(IRequest req, IResponse res, object responseDto)
+    {
+      res.AddHeader(HttpHeaders.CacheControl, "no-store,must-revalidate,no-cache,max-age=0");
+    }
+  }
+}
